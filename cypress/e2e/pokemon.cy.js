2fac38f4a053a3a3e2f1b68dcd52521df506c815
Omar Baltaji <omr.baltaji@gmail.com>
2025-07-07 16:51:12 +0300

    11.9

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -6,4 +6,14 @@ describe('Pokedex', () => {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate to a specific pokemon page', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('overgrow')
+    cy.contains('a', 'Previous').should('have.attr', 'href', '/pokemon/bulbasaur')
+    cy.contains('a', 'Next').should('have.attr', 'href', '/pokemon/venusaur')
+    cy.contains('chlorophyll')
+  })
 })
